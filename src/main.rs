298b00496d8a614bee9dd685112c4f54551c34c2
Ben Peoples <ben@benpeoples.com>
2022-02-04 14:24:16 -0500

    update: now using sets and prime factors!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use prime_tools;
+use std::collections::HashSet;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,11 +14,17 @@ fn main() {
 
     let target : u32 = args[1].parse().unwrap();
 
+    for target in 3..target {
+
     let mut target_set : Vec<u32> = Vec::new();
 
     for i in 1..(target+1) {
         target_set.push(i);
     }
+
+    // for target in 3..target {
+    
+    let mut the_set = HashSet::new();
 
     let mut ctr : usize = 0;
 
@@ -27,24 +34,41 @@ fn main() {
 
     let prime_list = prime_tools::get_primes_less_than_x(target+1);
 
-    // for i in target_set {
-    //     println!("{:#?}",prime_tools::get_prime_factors_with_counts(i.try_into().unwrap(),&prime_list));
-    // }
+    for i in &target_set {
+        let primes_with_counts = prime_tools::get_prime_factors_with_counts(*i,&prime_list);
+        // println!("{:#?}",primes_with_counts);
+        for p in primes_with_counts {
+            for i in 0..p.1 {
+                the_set.insert(format!("{}_{}",p.0,i));
+            }
+        }
+    }
+    // println!("{:#?}",the_set);
 
-    for i in prime_list {
-        println!("Adding {}", i);
-        test *= i;
+    for i in the_set {
+        let parts : Vec<&str> = i.split("_").collect();
+        let factor : u32 = parts[0].parse().unwrap();
+        test *= factor;
     }
 
-    test = test * (target - target%2)/2 * (target - target%3)/3;
+    println!("Target: {} Value: {}",target,test);
 
-    println!("Got: {}",test);
+    // for i in prime_list {
+    //     test *= i;
+    // }
+
+    // let n = target;
+
+    // test = test * (n - (n-1)%2)/2 * (n - (n-1)%3)/3;
+
+    // println!("Target {} Got: {}",target,test);
 
     for i in 1..(target+1) {
         if test % i != 0 {
             println!("Not divisible by {}", i);
         }
-
     }
+
+}
 
 }
